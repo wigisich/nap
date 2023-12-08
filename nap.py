1b97ea687fdb53d56ef7c8b9ecd2b79821e18ce0
wigisich <battuhanfidan@gmail.com>
2023-12-08 22:08:58 +0300

    add add_task/add_subtask with argparse

--- nap.py
+++ nap.py
@@ -7,8 +7,13 @@ parser = argparse.ArgumentParser(
             formatter_class = argparse.ArgumentDefaultsHelpFormatter,
         )
 
-parser.add_argument("-a", "--add-task", help="add a task/subtask")
-parser.add_argument("-d", "--deadline", help="set the deadline")
+parser.add_argument("-at", "--add-task", help="add a task")
+parser.add_argument("-ast", "--add-subtask", action="append", help="add a subtask")
+
+parser.add_argument("-end", "--deadline", help="set the deadline")
+parser.add_argument("-day", "--days", help="set deadline as n-days later")
+parser.add_argument("-hr", "--hours", help="set deadline as n-hours later")
+
 parser.add_argument("-t", "--task", help="select a task")
 parser.add_argument("-st", "--subtask", help="select a subtask of a task")
 
@@ -25,23 +30,16 @@ parser.add_argument("-fn", "--fnaps", help="finished naps")
 
 args = parser.parse_args()
 config = vars(args)
+print(config)
 
 class Nap:
-    def __init__(
-            self,
-            idx: int,
-            task: str,
-            subtasks: list = [],
-            deadline: tuple[int]|None = None, # dd-mm-yyyy
-            days: float|None = None,
-            hours: float|None = None,
-            ):
-        self.deadline = datetime.strptime(deadline, "%d-%m-%Y") if deadline else datetime.now()
-        self.deadline = self.deadline + timedelta(days=days) if days else self.deadline
-        self.deadline = self.deadline + timedelta(hours=hours) if hours else self.deadline
-        self.idx = idx
-        self.task = task
-        self.subtasks = subtasks
+    def __init__(self, **kwargs):
+        self.deadline = datetime.strptime(kwargs["deadline"], "%d-%m-%Y") if kwargs["deadline"] else datetime.now()
+        self.deadline = self.deadline + timedelta(days=float(kwargs["days"])) if kwargs["days"] else self.deadline
+        self.deadline = self.deadline + timedelta(hours=float(kwargs["hours"])) if kwargs["hours"] else self.deadline
+        self.idx = 0
+        self.task =  kwargs["add_task"]
+        self.subtasks = kwargs["add_subtask"]
         self.status = True
     def __repr__(self):
         limit = 12
@@ -57,16 +55,16 @@ class Nap:
         limit_x = int(x*.6)
         cut = True if len(self.task)>limit_task else False
         task = self.task[:limit_task if cut else None]+"..." if cut else self.task
-        task = f"[{self.deadline.strftime('%d-%m-%Y')}] - [ {task} ({self.idx})]\n"
+        task = f"  [{self.deadline.strftime('%d-%m-%Y')}] - [ {task} ({self.idx})]\n"
         if subtasks:
             for idx, subtask in enumerate(self.subtasks):
                 cut = True if len(subtask)>limit_task else False
                 subtask = subtask[:limit_task-4 if cut else None]+"..." if cut else subtask
-                subtask = f"\t\t - [ {subtask} ({idx})]\n"
+                subtask = f"  \t\t - [ {subtask} ({idx})]\n"
                 task = task + subtask
         return task
 
-naps = [Nap(idx=0, deadline="12-12-2022", task="Task to do", subtasks=["asdasdasdasdasdasdaasd asd asdas dasda sdas dasdasd asda sda sdasdasd ", "qweqew qwe qwe qewqweqwe qweq qweqwe qweqweqwe qweqweqw eqwqwe qweqweqwe"]), Nap(idx=1, task="Another very very long task to do and it is so long that I don't know what to do with this task which implies that I need a professional help from who knows how to do this task especially when you know nothing about how to do the task within a short time of period that is either lower than at least on hour or maybe two.")]
+naps = [Nap(days=None, hours=None, idx=0, deadline="12-12-2022", add_task="Task to do", add_subtask=["asdasdasdasdasdasdaasd asd asdas dasda sdas dasdasd asda sda sdasdasd ", "qweqew qwe qwe qewqweqwe qweq qweqwe qweqweqwe qweqweqw eqwqwe qweqweqwe"]), Nap(days=None, add_subtask=[], hours=None, idx=1, deadline=None, add_task="Another very very long task to do and it is so long that I don't know what to do with this task which implies that I need a professional help from who knows how to do this task especially when you know nothing about how to do the task within a short time of period that is either lower than at least on hour or maybe two.")]
 
 def display_naps(naps):
     print("\n__naps__:\n---------\n")
@@ -75,8 +73,10 @@ def display_naps(naps):
 
 
 if __name__ == "__main__":
+    if config["add_task"]:
+        nap = Nap(**config)
+        naps.append(nap)
     display_naps(naps)
-    if config["sort"]:
-        naps = sorted(naps, key=lambda x: x.deadline, reverse=True)
-        display_naps(naps)
+
+
 
